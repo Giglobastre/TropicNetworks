ad9ef9d3eb19c6a9bf4fbf937d971ca52e65c3aa
michelcladaece <38043532+michelcladaece@users.noreply.github.com>
2018-04-04 18:22:43 +0200

    algo forte connexité  & dfs

---
+++ V.1.2/include/Graphe.h
@@ -0,0 +1,51 @@
+#ifndef GRAPHE_H
+#define GRAPHE_H
+#include "Arete.h"
+#include <vector>
+#include <fstream>
+#include <iostream>
+#include <stack>
+
+using namespace std;
+
+class Graphe
+{
+    public:
+        Graphe();
+        Graphe(vector<vector<float> > adj);
+        virtual ~Graphe();
+        void AllocOrdre(int m_ordre);
+        void load(string nomfile); //Fonction récupérant les infos depuis le fichier du graphe
+        void afficheInfos(); //Fonction affichant les infos récupérées (fonction de test)
+        void destroy(int n); //Fonction "détruisant" un sommet du graphe (set la santé à 0)
+        void save(string nomfile); //Fonction de sauvegarde
+        //void forteconnexite(int s);
+        int temps();
+
+        void fillOrder(int i , stack<int> &Stack);
+
+        void dfs(int n);
+        void getAdjacents();
+        vector<vector<float> > getTranspose();
+        void Kosaraju(); //forte connexite
+
+
+        //void forteconnexite();
+
+    protected:
+
+    private:
+
+        //Attributs privés
+        int m_ordre;
+        int m_nbAretes;
+        vector<Sommet> m_Sommets; //Liste des sommets
+        vector<vector<float> > m_adjacences; //Matrice d'adjacence pondérée
+        string m_nom;//Nom du fichier contenant le graphe
+};
+
+#endif // GRAPHE_H
+
+
+
+
---
+++ V.1.2/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include "Graphe.h"
+
+using namespace std;
+
+int main()
+{
+    Graphe graphe;
+    string nomfile = "graphe_1.txt";
+    graphe.load(nomfile);
+    graphe.afficheInfos();
+    graphe.dfs(0);
+    graphe.Kosaraju();
+    //graphe.forteconnexite(1);
+   /* graphe.destroy(1);
+    graphe.destroy(0);
+    graphe.destroy(2);
+    graphe.afficheInfos();
+    //graphe.save(nomfile);*/
+    return 0;
+}
