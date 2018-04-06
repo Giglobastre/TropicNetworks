a4c7bbde2ad4c42396895b7bafda2de7f1058cdf
Kenny HUBER <36986213+Giglobastre@users.noreply.github.com>
2018-04-06 19:03:43 +0200

    Ajout des images et des fonction d'ajout et de suppr de sommet
    -ajout d'un sommet supp le premier
    -le programme crash au bout d'un temps aleatoire ??
    -suppretion marche pas

---
+++ V2.5/src/Sommet.cpp
@@ -0,0 +1,109 @@
+#include "Sommet.h"
+
+Sommet::Sommet()
+{
+    m_sante=0;
+    m_coeff=0;
+    m_nom="";
+    m_numero=0;
+    m_actif = false;
+    m_marque=false;
+}
+Sommet::Sommet(int sante,float coeff ,std::string nom , int numero)
+: m_sante(sante) , m_coeff(coeff) , m_nom(nom) , m_numero(numero)
+{
+
+}
+Sommet::~Sommet()
+{
+    //dtor
+}
+///getters
+int Sommet::getSante()
+{
+    return m_sante;
+}
+float Sommet::getCoeff()
+{
+    return m_coeff;
+}
+std::string Sommet::getNom()
+{
+    return m_nom;
+}
+int Sommet::getNum()
+{
+    return m_numero;
+}
+int Sommet::getPos_x()
+{
+    return m_x;
+}
+int Sommet::getPos_y()
+{
+    return m_y;
+}
+
+bool Sommet::getActif()
+{
+    return m_actif;
+}
+bool Sommet::getMarque()
+{
+    return m_marque;
+}
+
+BITMAP* Sommet::getImage()
+{
+    return getImage();
+}
+
+///setters
+void Sommet::setSante(int val)
+{
+    m_sante=val;
+}
+void Sommet::setCoeff(float val)
+{
+    m_coeff=val;
+}
+void Sommet::setNom(std::string val)
+{
+    m_nom=val;
+}
+void Sommet::setNum(int val)
+{
+    m_numero=val;
+}
+
+void Sommet::setActif(bool val)
+{
+    m_actif = val;
+}
+void Sommet::setMarque(bool val)
+{
+    m_marque = true;
+}
+bool Sommet::est_adj_a(Sommet s)
+{
+    return true;
+}
+void Sommet::setPos_x(int val)
+{
+    m_x=val;
+}
+void Sommet::setPos_y(int val)
+{
+    m_y=val;
+}
+void Sommet::setImage(std::string val)
+{
+    /*BITMAP * image;
+
+    image=pack_fopen(val,NULL);
+    if (!image)
+    {
+        allegro_message("erreur chargement");
+        exit(EXIT_FAILURE);
+    }*/
+}
